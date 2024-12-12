bb8689f0002105fb25f5599036f3826fe5fb6ce2
Anne van den Berg <143817131+itsMiniscule@users.noreply.github.com>
2024-12-12 23:53:42 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+</script>
+
+<h1 class="bg-lime-400">Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
